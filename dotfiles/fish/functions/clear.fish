8ed32f91a9439bf2dc4402c4fc1e899e5db7c327
Funkinmofo <143419294+Funkinmofo@users.noreply.github.com>
2023-09-12 04:35:01 -0700

    Delete dotfiles/fish/functions/clear.fish

--- dotfiles/fish/functions/clear.fish
+++
@@ -1,3 +0,0 @@
-function clear --wraps='clear; echo; seq 1 (tput cols) | sort -R | spark | lolcat; echo; echo' --description 'alias clear clear; echo; seq 1 (tput cols) | sort -R | spark | lolcat; echo; echo'
-  clear; echo; seq 1 (tput cols) | sort -R | spark | lolcat; echo; echo $argv; 
-end
