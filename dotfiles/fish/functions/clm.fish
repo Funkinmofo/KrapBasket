4f3e37def6777e330b3e73d9f93b1140a4ccc4d3
Funkinmofo <143419294+Funkinmofo@users.noreply.github.com>
2023-09-12 04:35:20 -0700

    Delete dotfiles/fish/functions/clm.fish

--- dotfiles/fish/functions/clm.fish
+++
@@ -1,3 +0,0 @@
-function clm --wraps='clear; echo; echo; seq 1 (tput cols) | sort -R | spark | lolcat; echo; echo && mach' --description 'alias clm clear; echo; echo; seq 1 (tput cols) | sort -R | spark | lolcat; echo; echo && mach'
-  clear; echo; echo; seq 1 (tput cols) | sort -R | spark | lolcat; echo; echo && mach $argv; 
-end
