5e56b64994a95521afed10067f309b7f1a14f5a0
Funkinmofo <143419294+Funkinmofo@users.noreply.github.com>
2023-09-12 04:35:33 -0700

    Delete dotfiles/fish/functions/clp.fish

--- dotfiles/fish/functions/clp.fish
+++
@@ -1,3 +0,0 @@
-function clp --wraps='clear; echo; echo; seq 1 (tput cols) | sort -R | spark | lolcat; echo; echo && mach && nwp' --description 'alias clp clear; echo; echo; seq 1 (tput cols) | sort -R | spark | lolcat; echo; echo && mach && nwp'
-  clear; echo; echo; seq 1 (tput cols) | sort -R | spark | lolcat; echo; echo && mach && nwp $argv; 
-end
