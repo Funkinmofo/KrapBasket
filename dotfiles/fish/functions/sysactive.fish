1d3bd322321ef4eb87ab7ac78e59be5b820ba97d
Funkinmofo <funkinmofo@gmail.com>
2023-09-19 19:39:03 -0700

    updates

---
+++ dotfiles/fish/functions/sysactive.fish
@@ -0,0 +1,4 @@
+function sysactive --wraps='syste,ctl --type=service --state=running | grep active' --wraps='systemctl --type=service --state=running | grep active' --description 'alias sysactive systemctl --type=service --state=running | grep active'
+  systemctl --type=service --state=running | grep active $argv
+        
+end
