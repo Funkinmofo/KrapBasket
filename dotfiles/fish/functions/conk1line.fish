0ba71d7c7cebfad671d8dd72083f01205261fde7
Funkinmofo <funkinmofo@gmail.com>
2023-09-22 03:43:58 -0700

    updates

--- dotfiles/fish/functions/conk1line.fish
+++ dotfiles/fish/functions/conk1line.fish
@@ -1,4 +1,4 @@
-function conk1line --wraps='conky -d -c /home/myrrdin/Documents/.conkyrc' --description 'alias conk1line conky -d -c /home/myrrdin/Documents/.conkyrc'
-  conky -d -c /home/myrrdin/Documents/.conkyrc $argv
+function conk1line --wraps='conky -d -c /home/myrrdin/Documents/.conkyrc' --wraps='conky -d -c /home/myrrdin/Documents/.conkyrc1line' --description 'alias conk1line conky -d -c /home/myrrdin/Documents/.conkyrc1line'
+  conky -d -c /home/myrrdin/Documents/.conkyrc1line $argv
         
 end
